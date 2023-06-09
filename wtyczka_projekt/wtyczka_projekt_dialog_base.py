781b33f1db46079de388f0a40383098f080b210a
JanSych <jan.sychowiec01@wp.pl>
2023-06-09 16:06:20 +0200

    poprawienie wysokosci

--- wtyczka_projekt/wtyczka_projekt_dialog_base.py
+++ wtyczka_projekt/wtyczka_projekt_dialog_base.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\ninaj\Desktop\Nauka\INF2\projekt_2_informatyka\wtyczka_projekt\wtyczka_projekt_dialog_base.ui'
+# Form implementation generated from reading ui file 'C:\Users\jansy\OneDrive\Pulpit\4_sem\Informatyka_II\projekt_2_informatyka\wtyczka_projekt\wtyczka_projekt_dialog_base.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
@@ -69,7 +69,7 @@ class Ui_WtyczkaProjektDialogBase(object):
         self.tytul.setHtml(_translate("WtyczkaProjektDialogBase", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt; font-weight:600; text-decoration: underline;\">Wtyczka do wykonywania operacji w programie QGIS</span></p></body></html>"))
         self.wybor_warstwy.setCurrentText(_translate("WtyczkaProjektDialogBase", "Wybierz warstwę"))
         self.klik_przewyzszenie.setText(_translate("WtyczkaProjektDialogBase", "Oblicz przewyższenie"))
